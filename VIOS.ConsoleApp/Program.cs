36492c70f62e19031bc49311d1cb756f7562b870
shresthabikash <shresthabikash1990@gmail.com>
2019-07-15 21:08:29 -0400

    Add project files.

---
+++ VIOS.ConsoleApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIOS.DataAccess;
+
+namespace VIOS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("VIOS Console Application");
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("1. Add New User");
+                Console.WriteLine("2. Update User");
+                Console.WriteLine("3. Delete User");
+                Console.WriteLine("4. Display All Users");
+                Console.WriteLine("5. Find user by Id");
+                Console.WriteLine("6. Exit");
+
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        DataAccess.DataAccess data = new DataAccess.DataAccess();
+                        UserLogin objUser = new UserLogin();
+                        Console.WriteLine("Enter new UserName");
+                        string UserName = Console.ReadLine();
+                        string Password = Console.ReadLine();
+                        objUser.UserName = UserName;
+                        objUser.UserPassword = Password;
+                        data.AddNewUser(objUser);
+                        
+                        Console.WriteLine("New User is Created");
+                        //Todo
+                        break;
+
+                    case 2:
+                        Console.WriteLine("User has been updated");
+                        //Todo
+                        break;
+                    case 3:
+
+                        Console.WriteLine("User has been Removed");
+                        //Todo
+                        break;
+
+                    case 4:
+                        Console.WriteLine("List of User");
+
+                        DataAccess.DataAccess data1 = new DataAccess.DataAccess();
+                        Console.WriteLine("UserName \tPassword");
+                        foreach (var item in data1.GetAllUser())
+                        {
+                            Console.WriteLine("{0}\t{1}", item.UserName, item.UserPassword);
+
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("User Info");
+                        //Todo
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        //Todo
+                        break;
+
+                }
+
+                Console.ReadKey();
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ VIOS.DataAccess/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIOS.DataAccess
+{
+    public class DataAccess
+    {
+        public void AddNewUser(UserLogin userLogin)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=192.168.0.53;Initial Catalog=VIOSDB;User ID=sa;password=innovative");
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("insert into UserLogin values('"+ userLogin.UserName+"','"+userLogin.UserPassword+"')", sqlConnection);
+            cmd.ExecuteNonQuery();
+        }
+
+        public List<UserLogin> GetAllUser()
+        {
+            List<UserLogin> userLogins = new List<UserLogin>();
+            SqlConnection sqlConnection = new SqlConnection("Data Source=192.168.0.53;Initial Catalog=VIOSDB;User ID=sa;password=innovative");
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("select * from UserLogin", sqlConnection);
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while(rd.Read())
+            {
+                userLogins.Add(new UserLogin()
+                {
+                    UserName = rd.GetString(0),
+                    UserPassword = rd.GetString(1)
+
+
+                }
+
+                    );
+
+            }
+            return userLogins;
+        }
+    }
+}
